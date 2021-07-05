0ba12b391f69e185a8af5ee4c770b1154efb0316
abhi <abhib0399@gmail.com>
2021-07-05 20:58:05 +0530

    11

--- 9.auto-text-effect/practise1/app.js
+++ 9.auto-text-effect/practise1/app.js
@@ -4,29 +4,30 @@ const h1=document.querySelector("h1")
 const minus=document.querySelector(".fa-minus")
 const plus=document.querySelector(".fa-plus")
 const speedInd=document.querySelector(".speedIndicator")
+const time=document.querySelector("p")
 
 let i=0;
-let spd=[1100,1000,900,800,700,600,500,400,300,100,50];
-let j=1;
+let spd=[1500,1300,1100,900,700,500,400,300,200,100,50];
+let j=0;
+let set=setTimeout;
 
+let speed=(spd)=> set=setTimeout(m1,spd);
+  
 let m1=()=>{
   
    h1.innerHTML+=`${Msg[i]}`
    i++;
+
    if(i==Msg.length+1){
-   clearInterval(set);
+   clearTimeout(set);
    i=0;
    h1.innerHTML=" "
    }
    
+   speed(spd[j])  
 }
-   let set=setInterval;
 
-   let speed=(spd)=>{
-     set=setInterval(m1,100);
-    }
-
-   speed(1000);
+  speed(spd[j]);
 
    plus.addEventListener("click",()=>{
       if(j>=0 && j<10){
@@ -34,14 +35,14 @@ let m1=()=>{
          speedInd.innerHTML=`${j}`
          speed(spd[j])
       }
-    
    })
 
    minus.addEventListener("click",()=>{
+      
       if(j>0 && j<=10){
          j--;
-         speedInd.innerHTML=`${j}`
-         speed(spd[j])
+         speedInd.innerHTML=`${j}`;
+         speed(spd[j]); 
       }
    })
 
